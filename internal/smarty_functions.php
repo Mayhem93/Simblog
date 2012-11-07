68b134d28a856f1f625933aeebded23fd1818816
Răzvan Botea <utherr.ghujax@gmail.com>
2012-11-07 18:21:23 +0200

    Removed some unused files/code.

--- internal/smarty_functions.php
+++ internal/smarty_functions.php
@@ -1,9 +1,5 @@
 <?php
 
-function smarty_isAdminSession() {
-	return isset($_SESSION[$_SERVER['REMOTE_ADDR']]['admin']);
-}
-
 function smarty_getSetting($setting) {
 	return SBFactory::Settings()->getSetting($setting);
 }
